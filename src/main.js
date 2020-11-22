4990ba16b59811199e7ddd2064f550a2d16985f2
Kati Ilus <kati.ilus1802@gmail.com>
2020-11-22 20:09:40 +0200

    Juhu

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Header from "@/components/Header";
+import Login from "@/components/Login";
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.use(Vuex);
+
+const routes = [
+  {path: '/', component: Header},
+  {path: '/login', component: Login}
+];
+const router = new VueRouter({routes});
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
